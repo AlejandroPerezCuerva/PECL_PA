655fe9094076da4296a3c2255d06ff4a8cc2709d
AlejandroPerezCuerva <48985427+AlejandroPerezCuerva@users.noreply.github.com>
2021-04-22 18:40:24 +0200

    creado vacunar ()

--- src/Sanitario.java
+++ src/Sanitario.java
@@ -1,6 +1,9 @@
 
+
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -41,7 +44,14 @@ public class Sanitario extends Thread {
     public void setContadoresSanitarios(ArrayList<AtomicInteger> contadoresSanitarios) {
         this.contadoresSanitarios = contadoresSanitarios;
     }
-
+    
+    public void vacunar(){
+        try {
+            this.sleep((int)(3000*Math.random()+2000));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Sanitario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     @Override
     public String toString() {
         return id;
