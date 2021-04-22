5ad367abd17b0d22466ac0b054274b4201086d1e
AlejandroPerezCuerva <48985427+AlejandroPerezCuerva@users.noreply.github.com>
2021-04-22 18:42:27 +0200

    operaciones con respecto a puestos

--- src/SalaObservacion.java
+++ src/SalaObservacion.java
@@ -1,5 +1,5 @@
 
-import java.util.ArrayList;
+iimport java.util.ArrayList;
 import javax.swing.JTextField;
 
 /**
@@ -8,22 +8,41 @@ import javax.swing.JTextField;
  */
 public class SalaObservacion {
 
-    private int max;
-    private ArrayList<JTextField> puestos= new ArrayList<JTextField>();
+    private int max, puestoLibre;
+    private ArrayList<Puesto> puestos= new ArrayList<Puesto>();
 
     public SalaObservacion(int max) {
         this.max = max;
         for (int i = 0; i < max; i++) {
-            puestos.add(new JTextField());
+            Puesto nuevoPuesto=  new Puesto(new JTextField(), true);
+            puestos.add(nuevoPuesto);
         }
+    }
+    
+    public ArrayList<Puesto> getPuestos(){
+        return puestos;
+    }
+    
+    public boolean puestoLibre(){
+        boolean resultado=false;
+        for (int i = 0; i < puestos.size(); i++) {
+            resultado=resultado | puestos.get(i).isDisponible();//falso o dispoible ->true, falso o no disponible ->false
+            if (resultado) 
+                puestoLibre=i;
+        }
+        return resultado;
     }
     
     public void entraPaciente(Paciente paciente){
         //procedimiento para que el paciente entre en la sala de observacion
+        if (puestoLibre()) {
+            puestos.get(puestoLibre).entraPaciente(paciente);
+        }
     }
     
-    public void salePaciente(Paciente paciente){
+    public void salePaciente(int puesto){
         //procedimiento para que el paciente se vaya a casa
-        System.out.println("Paciente: " + paciente.toString() + " sale del hospital");
+        System.out.println("Paciente: " +   puestos.get(puesto).toString() + " sale del hospital");
+        puestos.get(puesto).salePaciente();
     }
 }
